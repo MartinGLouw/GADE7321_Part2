bbf3757bccbedc647749f68c219d35bbb339093e
SpartanKick <ST10208767@vgconnect.edu.za>
2024-04-29 13:51:10 +0200

    Spawning fixed and board state

--- GADE7321_Part2/Assets/Scripts/Spawning.cs
+++ GADE7321_Part2/Assets/Scripts/Spawning.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawning : MonoBehaviour
@@ -12,116 +10,87 @@ public class Spawning : MonoBehaviour
     public GameObject puckblue;
     public GameObject puckred;
     public GameManager gameManager;
-    private bool isSpawning = false; 
-    private float spawnCooldown = 1.0f; 
+    private bool isSpawning = false;
+    private float spawnCooldown = 1.0f;
+    private bool Player1Turn = false;
+    private bool Player2Turn = false;
 
-    // Add a 2D array to represent the grid
-    private GameObject[,] grid = new GameObject[12, 12];
-    private void Update()
+    //New variables for board state
+    private int[,] boardState;
+    private int heightOfBoard = 12;
+    private int lengthOfBoard = 13;
+
+    private void Start()
     {
-        if (Input.GetMouseButtonDown(0) && !isSpawning)
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
-
-            if (hit.collider != null)
-            {
-                Debug.Log("Clicked on " + hit.collider.name);
-                string name = hit.collider.name;
-                if (name == "Column")
-                {
-                    ColumnIndex = 0;
-                }
-                else if (name.StartsWith("Column ("))
-                {
-                    string indexStr = name.Substring(8, name.Length - 9); // Extract the number between the parentheses
-                    ColumnIndex = int.Parse(indexStr);
-                }
-                StartCoroutine(SpawnColumn());
-            }
-        }
+        boardState = new int[lengthOfBoard, heightOfBoard];
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("Clicked" + ColumnIndex);
-        if (!isSpawning) 
+        if (!isSpawning && !IsColumnFull(ColumnIndex))
         {
             StartCoroutine(SpawnColumn());
         }
     }
 
-    public IEnumerator SpawnColumn() 
+    public IEnumerator SpawnColumn()
     {
-        isSpawning = true; 
-        GameObject puck;
-        int emptyRow = GetEmptyRow(ColumnIndex);
+        isSpawning = true;
         if (gameManager.turn == false)
         {
-            puck = Instantiate(puckblue, SpawnPoint.transform.position, Quaternion.identity);
+            Player2Turn = false;
+            Instantiate(puckblue, SpawnPoint.transform.position, Quaternion.identity);
             gameManager.turn = true;
+            Player1Turn = true;
         }
         else
         {
-            puck = Instantiate(puckred, SpawnPoint.transform.position, Quaternion.identity);
+            Player1Turn = false;
+            Instantiate(puckred, SpawnPoint.transform.position, Quaternion.identity);
             gameManager.turn = false;
+            Player2Turn = true;
         }
 
-        // Update the grid with the new puck
-        grid[ColumnIndex, emptyRow] = puck;
+        bool success = UpdateBoardState(ColumnIndex);
 
-        // Print out the state of the grid
-        Debug.Log("After spawning a puck at column " + ColumnIndex + " and row " + emptyRow + ":");
-        for (int i = 0; i < 12; i++)
+        if (!success)
         {
-            string row = "";
-            for (int j = 0; j < 12; j++)
+            Debug.Log($"Column {ColumnIndex} is full! Cannot spawn more pucks.");
+        }
+
+        yield return new WaitForSeconds(spawnCooldown);
+        isSpawning = false;
+    }
+
+    private bool IsColumnFull(int column)
+    {
+        for (int row = 0; row < heightOfBoard; row++)
+        {
+            if (boardState[column, row] == 0)
             {
-                if (grid[j, i] == null)
-                {
-                    row += "0 ";
-                }
-                else if (grid[j, i] == puckblue)
-                {
-                    row += "B ";
-                }
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool UpdateBoardState(int column)
+    {
+        for (int row = heightOfBoard - 1; row >= 0; row--)
+        {
+            if (boardState[column, row] == 0)
+            {
+                //Update board with current player's value
+                if (Player1Turn)
+                    boardState[column, row] = 1;
                 else
-                {
-                    row += "R ";
-                }
-            }
-            Debug.Log(row);
-        }
+                    boardState[column, row] = 2;
 
-        yield return new WaitForSeconds(spawnCooldown); 
-        isSpawning = false; 
-    }
-
-    // This function returns the index of the first empty row in the given column
-    private int GetEmptyRow(int columnIndex)
-    {
-        for (int i = 0; i < 12; i++)
-        {
-            if (grid[columnIndex, i] == null)
-            {
-                return i;
+                return true;
             }
         }
-        // If no empty row is found, return -1
-        return -1;
-    }
 
-    // Draw the grid using Gizmos
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.white;
-        Vector3 startPosition = new Vector3(-5.55999994f, -5.40417767f, 1.4626421e-06f);
-        for (int i = 0; i <= 12; i++)
-        {
-            // Draw vertical lines
-            Gizmos.DrawLine(startPosition + new Vector3(i, 0, 0), startPosition + new Vector3(i, 12, 0));
-            // Draw horizontal lines
-            Gizmos.DrawLine(startPosition + new Vector3(0, i, 0), startPosition + new Vector3(12, i, 0));
-        }
+        return false;
     }
 }
