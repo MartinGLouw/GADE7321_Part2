e9114c8e6027e8ad0008197ca9eb50f453894f99
SpartanKick <ST10208767@vgconnect.edu.za>
2024-05-01 12:04:01 +0200

    abilities

--- GADE7321_Part2/Assets/Scripts/GameManager.cs
+++ GADE7321_Part2/Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public bool turn = true; // Player 1's turn
 
-    
-
     private int[,] boardState;
     private int heightOfBoard = 12;
     private int lengthOfBoard = 12;
+
+    // Power-up tracking
+    private int[] player1Powerups = { 1, 1, 1 }; 
+    private int[] player2Powerups = { 1, 1, 1 }; 
+
+    private bool isPowerUpSelected = false;
+    private int selectedPowerup = 0; 
+    public int ColumnIndex; // Assuming Spawning sets this when a puck lands
 
     private void Start()
     {
@@ -50,7 +57,6 @@ public class GameManager : MonoBehaviour
             Debug.Log($"Player {currentPlayer} wins!");
         }
     }
-
     private bool CheckWinConditionForPlayer(int player)
     {
         // Check horizontal wins
@@ -119,10 +125,7 @@ public class GameManager : MonoBehaviour
 
         // No win condition found
         return false;
-    }
-
-
-    public int GetCurrentPlayer()
+    }public int GetCurrentPlayer()
     {
         return turn ? 1 : 2;
     }
@@ -130,5 +133,110 @@ public class GameManager : MonoBehaviour
     public void SwitchTurns()
     {
         turn = !turn;
+    }
+
+    public void UsePowerup(int powerupIndex) 
+    {
+        if (!turn || isPowerUpSelected) return;
+
+        int[] currentPlayerPowerups = turn ? player1Powerups : player2Powerups;
+        if (currentPlayerPowerups[powerupIndex - 1] > 0)
+        {
+            isPowerUpSelected = true;
+            selectedPowerup = powerupIndex;
+            currentPlayerPowerups[powerupIndex - 1]--; 
+        }
+    }
+    public bool GetCurrentPlayerHasPowerups()
+    {
+        int[] currentPlayerPowerups = turn ? player1Powerups : player2Powerups;
+        return currentPlayerPowerups.Any(count => count > 0);
+    }
+
+    public void EndTurn()
+    {
+        if (isPowerUpSelected)
+        {
+            ExecutePowerup();
+        } else
+        {
+            CheckWinCondition();
+            SwitchTurns();
+        }
+    }
+
+    private void ExecutePowerup()
+    {
+        switch (selectedPowerup)
+        {
+            case 1: 
+                DeleteHorizontalRow(GetAffectedRow());
+                break;
+            case 2:
+                DeleteVerticalColumn(GetAffectedColumn());
+                break;
+            case 3:
+                // Do nothing, turn is skipped
+                break;
+            default: 
+                Debug.LogError("Invalid power-up selection");
+                break;
+        }
+        isPowerUpSelected = false; 
+        selectedPowerup = 0;
+        SwitchTurns(); 
+    }
+
+    private void DeleteHorizontalRow(int rowToDelete)
+    {
+        if (rowToDelete >= 0)
+        {
+            for (int x = 0; x < lengthOfBoard; x++)
+            {
+                boardState[x, rowToDelete] = 0;
+            }
+
+            for (int x = 0; x < lengthOfBoard; x++)
+            {
+                ShiftColumnDown(x, rowToDelete);
+            }
+        }
+    }
+
+    private void DeleteVerticalColumn(int colToDelete)
+    {
+        if (colToDelete >= 0)
+        {
+            for (int y = 0; y < heightOfBoard; y++)
+            {
+                boardState[colToDelete, y] = 0;
+            }
+        }
+    }
+
+    private int GetAffectedColumn() 
+    {
+        return ColumnIndex; 
+    }
+
+    private int GetAffectedRow()
+    {
+        for (int y = heightOfBoard - 1; y >= 0; y--)
+        {
+            if (boardState[GetAffectedColumn(), y] == 0)
+            {
+                return y; 
+            }
+        }
+        return -1; 
+    }
+
+    private void ShiftColumnDown(int column, int deletedRow)
+    {
+        for (int y = deletedRow; y > 0; y--)
+        {
+            boardState[column, y] = boardState[column, y - 1];
+        }
+        boardState[column, 0] = 0; 
     }
 }
--- GADE7321_Part2/Assets/Scripts/Spawning.cs
+++ GADE7321_Part2/Assets/Scripts/Spawning.cs
@@ -10,15 +10,30 @@ public class Spawning : MonoBehaviour
     public GameObject puckblue;
     public GameObject puckred;
     public GameManager gameManager;
+    public GameObject powerupButton1; 
+    public GameObject powerupButton2;
+    public GameObject powerupButton3;
 
+    
+    private bool isSelectingPowerup = false; // Flag for power-up selection 
+    private int powerupIndexSelected = 0;
     private bool isSpawning = false;
     private float spawnCooldown = 1.0f;
 
     private void OnMouseDown()
     {
-        if (!isSpawning && !gameManager.IsColumnFull(ColumnIndex)) 
+        if (isSelectingPowerup) return; // Don't process clicks during power-up selection
+
+        if (!isSpawning && !gameManager.IsColumnFull(ColumnIndex))
         {
-            StartCoroutine(SpawnColumn());
+            if (gameManager.GetCurrentPlayerHasPowerups())
+            {
+                ShowPowerupSelectionUI(); 
+            }
+            else
+            {
+                StartCoroutine(SpawnColumn());
+            }
         }
     }
 
@@ -33,9 +48,51 @@ public class Spawning : MonoBehaviour
         {
             gameManager.CheckWinCondition();
             gameManager.SwitchTurns();
+            gameManager.EndTurn(); // End the turn
         }
 
         yield return new WaitForSeconds(spawnCooldown);
         isSpawning = false;
+    }
+    private void ShowPowerupSelectionUI()
+    {
+        powerupButton1.SetActive(true);
+        powerupButton2.SetActive(true);
+        powerupButton3.SetActive(true);
+        isSelectingPowerup = true;
+    }
+
+    // Call these functions from your power-up button's OnClick event
+    public void SelectPowerup1() 
+    {
+        powerupIndexSelected = 1;
+        UseSelectedPowerup();
+    }
+
+    public void SelectPowerup2()
+    {
+        powerupIndexSelected = 2;
+        UseSelectedPowerup();
+    }
+
+    public void SelectPowerup3()
+    {
+        powerupIndexSelected = 3;
+        UseSelectedPowerup();
+    }
+
+    private void UseSelectedPowerup()
+    {
+        HidePowerupSelectionUI(); 
+        gameManager.UsePowerup(powerupIndexSelected);
+        powerupIndexSelected = 0;
+    }
+
+    private void HidePowerupSelectionUI()
+    {
+        powerupButton1.SetActive(false);
+        powerupButton2.SetActive(false);
+        powerupButton3.SetActive(false);
+        isSelectingPowerup = false;
     }
 }
