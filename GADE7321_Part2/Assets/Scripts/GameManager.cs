8cce4e95ef58b1dd2cce237af977e4a39d4d00b2
SpartanKick <ST10208767@vgconnect.edu.za>
2024-05-01 11:45:02 +0200

    spawn works

--- GADE7321_Part2/Assets/Scripts/GameManager.cs
+++ GADE7321_Part2/Assets/Scripts/GameManager.cs
@@ -4,5 +4,131 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool turn = false;
+    public bool turn = true; // Player 1's turn
+
+    
+
+    private int[,] boardState;
+    private int heightOfBoard = 12;
+    private int lengthOfBoard = 12;
+
+    private void Start()
+    {
+        boardState = new int[lengthOfBoard, heightOfBoard];
+    }
+
+    public bool IsColumnFull(int column)
+    {
+        for (int row = 0; row < heightOfBoard; row++)
+        {
+            if (boardState[column, row] == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public bool UpdateBoardState(int column)
+    {
+        for (int row = 0; row < heightOfBoard; row++)
+        {
+            if (boardState[column, row] == 0)
+            {
+                boardState[column, row] = turn ? 1 : 2;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void CheckWinCondition()
+    {
+        int currentPlayer = turn ? 1 : 2;  
+        if (CheckWinConditionForPlayer(currentPlayer))
+        {
+            Debug.Log($"Player {currentPlayer} wins!");
+        }
+    }
+
+    private bool CheckWinConditionForPlayer(int player)
+    {
+        // Check horizontal wins
+        for (int x = 0; x < lengthOfBoard - 4; x++) // Changed from -3 to -4
+        {
+            for (int y = 0; y < heightOfBoard; y++)
+            {
+                if (boardState[x, y] == player &&
+                    boardState[x + 1, y] == player &&
+                    boardState[x + 2, y] == player &&
+                    boardState[x + 3, y] == player && 
+                    boardState[x + 4, y] == player)  // Check for 5 in a row
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Check vertical wins
+        for (int x = 0; x < lengthOfBoard; x++)
+        {
+            for (int y = 0; y < heightOfBoard - 4; y++) // Changed from -3 to -4
+            {
+                if (boardState[x, y] == player &&
+                    boardState[x, y + 1] == player &&
+                    boardState[x, y + 2] == player &&
+                    boardState[x, y + 3] == player &&
+                    boardState[x, y + 4] == player) // Check for 5 in a row
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Check diagonal wins (top-left to bottom-right)
+        for (int x = 0; x < lengthOfBoard - 4; x++) // Changed from -3 to -4
+        {
+            for (int y = 0; y < heightOfBoard - 4; y++) // Changed from -3 to -4
+            {
+                if (boardState[x, y] == player &&
+                    boardState[x + 1, y + 1] == player &&
+                    boardState[x + 2, y + 2] == player &&
+                    boardState[x + 3, y + 3] == player &&
+                    boardState[x + 4, y + 4] == player) // Check for 5 in a row
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Check diagonal wins (bottom-left to top-right)
+        for (int x = 0; x < lengthOfBoard - 4; x++)  // Changed from -3 to -4
+        {
+            for (int y = 4; y < heightOfBoard; y++) // Changed from 3 to 4
+            {
+                if (boardState[x, y] == player &&
+                    boardState[x + 1, y - 1] == player &&
+                    boardState[x + 2, y - 2] == player &&
+                    boardState[x + 3, y - 3] == player &&
+                    boardState[x + 4, y - 4] == player) // Check for 5 in a row
+                {
+                    return true;
+                }
+            }
+        }
+
+        // No win condition found
+        return false;
+    }
+
+
+    public int GetCurrentPlayer()
+    {
+        return turn ? 1 : 2;
+    }
+
+    public void SwitchTurns()
+    {
+        turn = !turn;
+    }
 }
--- GADE7321_Part2/Assets/Scripts/Spawning.cs
+++ GADE7321_Part2/Assets/Scripts/Spawning.cs
@@ -10,24 +10,13 @@ public class Spawning : MonoBehaviour
     public GameObject puckblue;
     public GameObject puckred;
     public GameManager gameManager;
+
     private bool isSpawning = false;
     private float spawnCooldown = 1.0f;
-    private bool Player1Turn = false;
-    private bool Player2Turn = false;
-
-    //New variables for board state
-    private int[,] boardState;
-    private int heightOfBoard = 12;
-    private int lengthOfBoard = 12;
-
-    private void Start()
-    {
-        boardState = new int[lengthOfBoard, heightOfBoard];
-    }
 
     private void OnMouseDown()
     {
-        if (!isSpawning && !IsColumnFull(ColumnIndex))
+        if (!isSpawning && !gameManager.IsColumnFull(ColumnIndex)) 
         {
             StartCoroutine(SpawnColumn());
         }
@@ -36,149 +25,17 @@ public class Spawning : MonoBehaviour
     public IEnumerator SpawnColumn()
     {
         isSpawning = true;
-        if (gameManager.turn == false)
-        {
-            Player2Turn = false;
-            Instantiate(puckblue, SpawnPoint.transform.position, Quaternion.identity);
-            gameManager.turn = true;
-            Player1Turn = true;
-        }
-        else
-        {
-            Player1Turn = false;
-            Instantiate(puckred, SpawnPoint.transform.position, Quaternion.identity);
-            gameManager.turn = false;
-            Player2Turn = true;
-        }
 
-        bool success = UpdateBoardState(ColumnIndex);
+        GameObject puckToSpawn = gameManager.GetCurrentPlayer() == 1 ? puckblue : puckred;
+        Instantiate(puckToSpawn, SpawnPoint.transform.position, Quaternion.identity);
 
-        if (success)
+        if (gameManager.UpdateBoardState(ColumnIndex))
         {
-            int currentPlayer = Player1Turn ? 1 : 2;
-            if (CheckWinCondition(currentPlayer))
-            {
-                Debug.Log($"Player {currentPlayer} wins!");
-            }
+            gameManager.CheckWinCondition();
+            gameManager.SwitchTurns();
         }
 
         yield return new WaitForSeconds(spawnCooldown);
         isSpawning = false;
     }
-
-    private bool IsColumnFull(int column)
-    {
-
-        for (int row = 0; row < heightOfBoard; row++)
-        {
-            if (boardState[column, row] == 0)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private bool UpdateBoardState(int column)
-    {
-
-        for (int row = 0; row < heightOfBoard; row++)
-        {
-            if (boardState[column, row] == 0)
-            {
-                //Update board with current player's value
-                if (Player1Turn)
-                    boardState[column, row] = 1;
-                else
-                    boardState[column, row] = 2;
-
-
-                Debug.Log("Updated board state" + (column) + " " + (row));
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private bool CheckWinCondition(int player)
-    {
-        //Check horizontal cells
-        
-        for (int x = 0; x < lengthOfBoard - 6; x++)
-        {
-            for (int y = 0; y < heightOfBoard; y++)
-            {
-                if (boardState[x, y] == player &&
-                    boardState[x + 1, y] == player &&
-                    boardState[x + 2, y] == player &&
-                    boardState[x + 3, y] == player)
-                {Debug.Log("win");
-                    
-                    return true;
-                    
-                }
-
-            }
-        }
-
-        //vertical check
-
-        for (int x = 0; x < lengthOfBoard; x++)
-        {
-            for (int y = 0; y < heightOfBoard-6; y++)
-            {
-                if (boardState[x, y] == player &&
-                    boardState[x, y + 1] == player &&
-                    boardState[x, y + 2] == player &&
-                    boardState[x, y + 3] == player)
-                {
-                    return true;
-                }
-            }
-
-
-        }
-        //Diagonal check
-        for (int x = 0; x < lengthOfBoard - 6; x++)
-        {
-            for (int y = 0; y < heightOfBoard - 6; y++)
-            {
-                if (boardState[x, y] == player &&
-                    boardState[x + 1, y + 1] == player &&
-                    boardState[x + 2, y + 2] == player &&
-                    boardState[x + 3, y + 3] == player)
-                {
-                    return true;
-                }
-            }
-
-
-        }
-        //Diagonal check
-        for (int x = 0; x < lengthOfBoard - 6; x++)
-        {
-            for (int y = 0; y < heightOfBoard - 6; y++)
-            {
-                if (boardState[x, y + 3] == player &&
-                    boardState[x + 1, y + 2] == player &&
-                    boardState[x + 2, y + 1] == player &&
-                    boardState[x + 3, y] == player)
-                {
-                    return true;
-                }
-            }
-
-
-        }
-        return false;
-    }
 }
-    
-
-
-
-    
-
-
